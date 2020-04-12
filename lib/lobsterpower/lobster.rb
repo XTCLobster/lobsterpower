01337cf92b54e343a7297eae29d2fb7b5030ddae
Christian Tietze <christian.tietze@gmail.com>
2020-04-12 18:26:09 +0200

    fix lobster entity size

--- lib/lobsterpower/lobster.rb
+++ lib/lobsterpower/lobster.rb
@@ -9,7 +9,7 @@ module LobsterPower
     TARGET_SIZE = [100, 100]
 
     def initialize(x = 0, y = 0)
-      @game_object = MiniGL::GameObject.new x, y, IMAGE_SIZE[0], IMAGE_SIZE[1], :lobster
+      @game_object = MiniGL::GameObject.new x, y, TARGET_SIZE[0], TARGET_SIZE[1], :lobster
       # Compute the drawing scales in X and Y direction: 100/520 = 0.1923
       @scales = IMAGE_SIZE.zip(TARGET_SIZE)
         .map { |rel| rel[1].to_f / rel[0].to_f }
